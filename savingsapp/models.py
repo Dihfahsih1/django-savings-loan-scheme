91a5336a861fc7c6701821a3974ef6c8325054b6
Dihfahsih1 <dihfahsihm@gmail.com>
2020-04-18 17:59:57 +0300

    Attendance Model

--- savingsapp/models.py
+++ savingsapp/models.py
@@ -10,3 +10,12 @@ class CustomUser(AbstractUser):
     application_fee = models.IntegerField(default=10000, blank=True, null=True)
     def __str__(self):
         return self.first_name + ' ' + self.last_name
+
+class Attendance(models.Model):
+    atte =(('P','Present'),('A','Absent'))
+    date = models. DateField(blank=False)
+    name = models.ForeignKey(CustomUser,blank=True, on_delete=models.CASCADE)
+    status = models.CharField(max_length=123, blank=False, null=False, choices=atte)
+    social_fund = models.IntegerField(default=0, blank=True, null=True)
+    def __str__(self):
+        return self.Name
