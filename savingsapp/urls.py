a19fcd4198b9c37e463c4b43df00e1229c724981
Dihfahsih1 <dihfahsihm@gmail.com>
2020-04-18 12:27:14 +0300

    add urls

--- savingsapp/urls.py
+++ savingsapp/urls.py
@@ -3,4 +3,13 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^Savings-add-member', views.add_member, name='add-member'),
+    url(r'^Savings-members-list', views.members_list, name='members-list'),
+    url(r'^Savings-loan-application', views.loan_application, name='loan-application'),
+    url(r'^Savings-make-attendence', views.make_attendence, name='make-attendence'),
+    url(r'^Savings-attendence-history', views.attendence_history, name='attendence-history'),
+    url(r'^Savings-make-saving', views.make_saving, name='make-saving'),
+    url(r'^Savings-savings-list', views.savings_list, name='savings-list'),
+    url(r'^Savings-pay-loan', views.pay_loan, name='pay-loan'),
+    url(r'^Savings-social-fund', views.social_fund, name='social-fund'),
     ]
