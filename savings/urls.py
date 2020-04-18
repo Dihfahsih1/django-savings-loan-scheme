91e327434333cd70deb6cec335effecd6c9c1533
Dihfahsih1 <dihfahsihm@gmail.com>
2020-04-18 12:16:31 +0300

    fxing the error of urls.py instead of url.py

--- savings/urls.py
+++ savings/urls.py
@@ -2,14 +2,12 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include,path
 from django.contrib.auth.views import *
-from savingsapp import views
 from django.conf.urls import url
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^$', views.index, name='index'),
+    path('', include('savingsapp.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- savingsapp/url.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, include
-from django.contrib.auth.views import *
-from django.urls import path
-from savingsapp import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='/'),
-    ]
