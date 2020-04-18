085f446fb5176dc4e1af53aa782c3ef7b4078d86
Dihfahsih1 <dihfahsihm@gmail.com>
2020-04-18 13:16:42 +0300

    add more fields on the user model ofcourse with form

--- savingsapp/models.py
+++ savingsapp/models.py
@@ -2,4 +2,10 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 class CustomUser(AbstractUser):
     email = models.EmailField(max_length=255, unique=True, blank=True, null=True)
-    username = models.CharField(max_length=30, unique=True)
+    username = models.CharField(max_length=30, unique=True,blank=True, null=True)
+    telephone = models.IntegerField(default=0, blank=True, null=True)
+    first_name = models.CharField(max_length=30, blank=True, null=True)
+    last_name = models.CharField(max_length=255, blank=True, null=True)
+    username = models.CharField(max_length=30, unique=True, blank=True, null=True)
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
