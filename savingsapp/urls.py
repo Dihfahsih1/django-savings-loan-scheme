9a88b97eddb3f9956c4212b295ae9db35c40e9ba
Dihfahsih1 <dihfahsihm@gmail.com>
2020-04-20 12:02:15 +0300

    view member

--- savingsapp/urls.py
+++ savingsapp/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^Savings-add-member', views.add_member, name='add-member'),
     url(r'^Savings-edit-member/(?P<pk>\d+)', views.edit_member, name='edit-member'),
+    url(r'^Savings-view-member/(?P<pk>\d+)', views.view_member, name='view-member'),
     url(r'^Savings-delete-member/(?P<pk>\d+)', views.delete_member, name='delete-member'),
     url(r'^Savings-members-list', views.members_list, name='members-list'),
     url(r'^Savings-make-attendence', views.make_attendence, name='make-attendence'),
--- savingsapp/views.py
+++ savingsapp/views.py
@@ -45,6 +45,13 @@ def members_list(request):
     }
     return render(request,'members_list.html', context)
 
+def view_member(request, pk):
+    all_members=CustomUser.objects.filter(id=pk)
+    context = {
+    'member_details':member_details
+    }
+    return render(request,'view_member.html' context)
+
 def give_loan(request):
     return render(request,'loan_application.html')
 def pay_loan(request):
