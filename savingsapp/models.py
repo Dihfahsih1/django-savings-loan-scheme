b6535f86fa2f91a01ca68e6d20d54bbe6bcf29dd
Dihfahsih1 <dihfahsihm@gmail.com>
2020-05-12 22:23:03 +0300

    changes to custom model, remove duplicate field

--- savingsapp/models.py
+++ savingsapp/models.py
@@ -6,7 +6,6 @@ from django.db.models import Sum
 class CustomUser(AbstractUser):
 	roles =(('Admin','Admin'),('Ordinary','Ordinary'))
 	email = models.EmailField(max_length=255, unique=True, blank=True, null=True)
-	username = models.CharField(max_length=30, unique=True,blank=True, null=True)
 	telephone = models.IntegerField(default=0, blank=True, null=True)
 	first_name = models.CharField(max_length=30, blank=True, null=True)
 	last_name = models.CharField(max_length=255, blank=True, null=True)
