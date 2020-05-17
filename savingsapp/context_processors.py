83139b95bfd4e155a0c8fedaf1cb4b19853b9772
Dihfahsih1 <dihfahsihm@gmail.com>
2020-05-17 11:48:17 +0300

    add datetime to the context_processor

--- savingsapp/context_processors.py
+++ savingsapp/context_processors.py
@@ -1,5 +1,11 @@
 from .models import *
-
+from .views import *
 def cycle_processor(request):
+    today = today = datetime.now()
     currentCycle = Cycle.objects.get(is_active=True)
-    return{'currentCycle': currentCycle}
+    context = {
+        'currentCycle': currentCycle,
+        'today':today
+
+    }
+    return context
