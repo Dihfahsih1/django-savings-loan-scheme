d7c56d6e446fd6242b2d6dc3d33517f39e4cc241
Dihfahsih1 <dihfahsihm@gmail.com>
2020-05-24 05:11:17 +0300

    return else part of it

--- savingsapp/context_processors.py
+++ savingsapp/context_processors.py
@@ -2,8 +2,10 @@ from .models import *
 from .views import *
 def cycle_processor(request):
     today = today = datetime.now()
-    currentCycle = Cycle.objects.get(is_active=True)
-   
+    if Cycle.objects.get(is_active=True):
+        currentCycle = Cycle.objects.get(is_active=True)
+    else:
+        currentCycle='2020'
 
     context = {
         'currentCycle': currentCycle,
