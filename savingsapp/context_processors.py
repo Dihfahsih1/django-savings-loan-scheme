4241aa0bf35b2733a04999e6a0405f8f68ac123b
Dihfahsih1 <dihfahsihm@gmail.com>
2020-05-24 05:19:13 +0300

    hardcording

--- savingsapp/context_processors.py
+++ savingsapp/context_processors.py
@@ -2,10 +2,11 @@ from .models import *
 from .views import *
 def cycle_processor(request):
     today = today = datetime.now()
-    if Cycle.objects.get(is_active=True).exist():
-        currentCycle = Cycle.objects.get(is_active=True)
-    else:
-        currentCycle='2020/2021'
+    currentCycle = '2020/2021'
+    # if Cycle.objects.get(is_active=True).exist():
+    #     currentCycle = Cycle.objects.get(is_active=True)
+    # else:
+    #     currentCycle='2020/2021'
 
     context = {
         'currentCycle': currentCycle,
