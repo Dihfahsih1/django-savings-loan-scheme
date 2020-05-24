80566b12dcd9b230bc96f51e28133a710f7179b2
Dihfahsih1 <dihfahsihm@gmail.com>
2020-05-24 14:35:28 +0300

    remove testing code

--- savingsapp/context_processors.py
+++ savingsapp/context_processors.py
@@ -2,7 +2,6 @@ from .models import *
 from .views import *
 def cycle_processor(request):
     today = today = datetime.now()
-    currentCycle = '2020/2021'
     if Cycle.objects.get(is_active=True).exist():
         currentCycle = Cycle.objects.get(is_active=True)
     else:
